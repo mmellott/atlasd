f458dafd057221f4d74f82267b0dee34a788b2c2
mmellott <matthew.mellott@yahoo.com>
2016-04-06 02:25:48 -0700

    start to use libnl

--- atlasd.c
+++ atlasd.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 #include <netlink/netlink.h>
+#include <netlink/socket.h>
+#include <netlink/msg.h>
 
 int main()
 {
+    struct nl_sock *sk = NULL;
+    struct nl_msg *msg = NULL;
+
+    sk = nl_socket_alloc();
+    msg = nlmsg_alloc();
+
     printf("Goodbye, cruel world...\n");
+
+fail:
+    if (sk) nl_socket_free(sk);
+    if (msg) nlmsg_free(msg);
 }
 
