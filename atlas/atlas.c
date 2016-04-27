ccc0d5d834e0b54af08ee6c9320378350cf45372
mmellott <matthew.mellott@yahoo.com>
2016-04-26 22:52:51 -0700

    set cw hopefully

--- atlas/atlas.c
+++ atlas/atlas.c
@@ -1,12 +1,14 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/string.h>
 
 #include <net/cfg80211.h>
+#include <net/mac80211.h>
 #include <linux/netdevice.h>
 
-struct ieee80211_txq_params p;
-struct wiphy *w;
+void print_net_dev(void);
+void set_cw(struct net_device *dev);
 
 void print_net_dev(void)
 {
@@ -14,15 +16,34 @@ void print_net_dev(void)
 
 	dev = first_net_device(&init_net);
 	while (dev) {
-	    printk(KERN_INFO "atlas: found [%s]\n", dev->name);
-	    dev = next_net_device(dev);
-	}
+		printk(KERN_INFO "atlas: found [%s]\n", dev->name);
+		if (strcmp(dev->name, "wlan0")) {
+			set_cw(dev);
+		}
 
+		dev = next_net_device(dev);
+	}
+}
+
+void set_cw(struct net_device *dev)
+{
+	struct ieee80211_txq_params txq_params;
+	int ac, result;
+
+	printk(KERN_INFO "atlas: setting cw\n");
+
+	for (ac = 0; ac < IEEE80211_NUM_ACS; ac++) {
+		txq_params.ac = ac;
+		txq_params.txop = 0;
+		txq_params.cwmin = 1;
+		txq_params.cwmax = 7;
+		txq_params.aifs = 2;
+		result = set_txq_params(dev->ieee80211_ptr->wiphy, &txq_params);
+	}
 }
 
 static int __init hello_init(void)
 {
-	//w = wiphy_new(NULL, 0);
 	print_net_dev();
 	return 0;
 }
