35883667a375d4b9a4b027d8792749e5220cf043
mmellott <matthew.mellott@yahoo.com>
2016-04-26 21:46:58 -0700

    list all net_device

--- atlas/atlas.c
+++ atlas/atlas.c
@@ -3,13 +3,27 @@
 #include <linux/init.h>
 
 #include <net/cfg80211.h>
+#include <linux/netdevice.h>
 
 struct ieee80211_txq_params p;
 struct wiphy *w;
 
+void print_net_dev(void)
+{
+	struct net_device *dev;
+
+	dev = first_net_device(&init_net);
+	while (dev) {
+	    printk(KERN_INFO "atlas: found [%s]\n", dev->name);
+	    dev = next_net_device(dev);
+	}
+
+}
+
 static int __init hello_init(void)
 {
-        w = wiphy_new(NULL, 0);
+	//w = wiphy_new(NULL, 0);
+	print_net_dev();
 	return 0;
 }
 
